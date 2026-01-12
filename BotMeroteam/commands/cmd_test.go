dd8bce9bad54d82c50deb858683a871177d9f611
SIKO-1 <156326838+SIKO-1@users.noreply.github.com>
2026-01-12 06:47:31 +0300

    Update cmd_test.go

--- BotMeroteam/commands/cmd_test.go
+++ BotMeroteam/commands/cmd_test.go
@@ -1,13 +1,10 @@
-package commands
+package main
 
 import (
-    "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func HandleTest(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-    if update.Message.Text != "اختبار" { // بدون شخطه
-        return
-    }
-
-    bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "✅ البوت شغال!"))
+func Handle(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+    msg := tgbotapi.NewMessage(update.Message.Chat.ID, "✅ البوت شغال وسريع!")
+    bot.Send(msg)
 }
