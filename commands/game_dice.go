c086b7f381ed970643b40829aa13d2ea9eab3636
SIKO-1 <156326838+SIKO-1@users.noreply.github.com>
2026-01-12 06:38:18 +0300

    Update game_dice.go

--- commands/game_dice.go
+++ commands/game_dice.go
@@ -1,19 +1,21 @@
 package commands
 
 import (
+    "fmt"
     "math/rand"
     "time"
 
-    "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func DiceGame(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
-    if message.Text != "/نرد" {
+func HandleDice(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+    if update.Message.Text != "نرد" { // بدون شخطه
         return
     }
 
     rand.Seed(time.Now().UnixNano())
     value := rand.Intn(6) + 1
-    msg := tgbotapi.NewMessage(message.Chat.ID, "🎲 رمية النرد: "+string(rune(value+'0')))
-    bot.Send(msg)
+
+    msg := fmt.Sprintf("🎲 رميت النرد وطلعت: %d", value)
+    bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
 }
