36b662341c3d85eb29f8060c0354ca5416f2f614
SIKO-1 <156326838+SIKO-1@users.noreply.github.com>
2026-01-12 15:24:29 +0300

    Update game_dice.go

--- BotMeroteam/commands/game_dice.go
+++ BotMeroteam/commands/game_dice.go
@@ -1,19 +1,10 @@
-package main
+package commands
 
 import (
-    "math/rand"
-    "time"
-
-    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func Handle(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-    rand.Seed(time.Now().UnixNano())
-    value := rand.Intn(6) + 1
-
-    text := "🎲 رمي النرد...\n"
-    text += "الحظ يبتسم لك! الرقم: " + string(rune('0'+value))
-
-    msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
-    bot.Send(msg)
+func GameDice(bot *tgbotapi.BotAPI, msg *tgbotapi.Message) {
+	dice := tgbotapi.NewDice(msg.Chat.ID)
+	bot.Send(dice)
 }
