db4c6b563b72e0553df1de103d44d500963f8c91
SIKO-1 <156326838+SIKO-1@users.noreply.github.com>
2026-01-12 15:31:21 +0300

    Delete BotMeroteam directory

--- BotMeroteam/commands/cmd_test.go
+++
@@ -1,8 +0,0 @@
-package commands
-
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-
-func CmdTest(bot *tgbotapi.BotAPI, msg *tgbotapi.Message) {
-	reply := tgbotapi.NewMessage(msg.Chat.ID, "⚡ البوت شغال ويرد فوراً")
-	bot.Send(reply)
-}
