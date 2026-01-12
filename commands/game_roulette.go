7d1ef0df71aa3e4c06b7953e4b8f57a2d6e2cbbe
SIKO-1 <156326838+SIKO-1@users.noreply.github.com>
2026-01-12 06:38:37 +0300

    Update game_roulette.go

--- commands/game_roulette.go
+++ commands/game_roulette.go
@@ -1,21 +1,38 @@
 package commands
 
 import (
+    "fmt"
     "math/rand"
     "time"
 
-    "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func RouletteGame(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
-    if message.Text != "/روليت" {
+func HandleRoulette(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+    if update.Message.Text != "روليت" { // بدون شخطه
         return
     }
 
     rand.Seed(time.Now().UnixNano())
-    outcomes := []string{"💰 فوز!", "💀 خسارة!", "🎉 جاكبوت!"}
-    result := outcomes[rand.Intn(len(outcomes))]
+    outcomes := []string{"خسارة", "فوز", "جاكبوت"}
+    weights := []int{50, 45, 5} // احتمالات تقريبة
 
-    msg := tgbotapi.NewMessage(message.Chat.ID, "🎰 روليت: "+result)
-    bot.Send(msg)
+    total := 0
+    for _, w := range weights {
+        total += w
+    }
+
+    roll := rand.Intn(total)
+    var result string
+    sum := 0
+    for i, w := range weights {
+        sum += w
+        if roll < sum {
+            result = outcomes[i]
+            break
+        }
+    }
+
+    msg := fmt.Sprintf("🎰 نتيجة الروليت: %s", result)
+    bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
 }
